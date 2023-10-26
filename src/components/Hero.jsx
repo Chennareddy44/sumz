10954a52a46b3bf80f677a04ab80a5d2ba99357e
Chenna Reddy <92796914+Chennareddy44@users.noreply.github.com>
2023-10-26 12:09:25 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
         <span className="orange_gradient ">OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
-        Simplify your reading with Summize, an open-source article summarizer
+        Simplify your reading with sumz, an open-source article summarizer
         that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
